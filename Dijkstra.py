bc43f4621b60eca58131cb635ae04b689eaa39b8
Pedro-Hack <62391255+Pedro-Hack@users.noreply.github.com>
2020-06-03 13:56:27 -0500

    Create Dijkstra.py

---
+++ Dijkstra.py
@@ -0,0 +1,112 @@
+# Primero, las importaciones y los formatos de datos. Las implementaciones originales 
+# sugieren el uso de namedtuple para almacenar datos de borde. Haremos exactamente eso,
+# pero agregaremos un valor predeterminado al argumento de costo.
+
+# Inicialicemos nuestros datos:
+
+from collections import deque, namedtuple
+
+# we'll use infinity as a default distance to nodes.
+inf = float('inf')
+Edge = namedtuple('Edge', 'start, end, cost')
+
+def make_edge(start, end, cost=1):
+  return Edge(start, end, cost)
+
+class Graph:
+    def __init__(self, edges):
+        # let's check that the data is right
+        wrong_edges = [i for i in edges if len(i) not in [2, 3]]
+        if wrong_edges:
+            raise ValueError('Wrong edges data: {}'.format(wrong_edges))
+
+        self.edges = [make_edge(*edge) for edge in edges]
+        
+# Encontremos los vértices. En la implementación original, los vértices se definen en _ _ init _ _, 
+# pero necesitaremos que se actualicen cuando cambien los bordes, por lo que los convertiremos en 
+# una propiedad, se volverán a contar cada vez que abordemos la propiedad. Probablemente no sea la 
+# mejor solución para gráficos grandes, pero para los pequeños irá.
+
+    @property
+    def vertices(self):
+        return set(
+            sum(
+                ([edge.start, edge.end] for edge in self.edges), []
+            )
+        )
+
+    # Ahora, agreguemos la funcionalidad de agregar y quitar.
+    
+    def get_node_pairs(self, n1, n2, both_ends=True):
+        if both_ends:
+            node_pairs = [[n1, n2], [n2, n1]]
+        else:
+            node_pairs = [[n1, n2]]
+        return node_pairs
+
+    def remove_edge(self, n1, n2, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        edges = self.edges[:]
+        for edge in edges:
+            if [edge.start, edge.end] in node_pairs:
+                self.edges.remove(edge)
+
+    def add_edge(self, n1, n2, cost=1, both_ends=True):
+        node_pairs = self.get_node_pairs(n1, n2, both_ends)
+        for edge in self.edges:
+            if [edge.start, edge.end] in node_pairs:
+                return ValueError('Edge {} {} already exists'.format(n1, n2))
+
+        self.edges.append(Edge(start=n1, end=n2, cost=cost))
+        if both_ends:
+            self.edges.append(Edge(start=n2, end=n1, cost=cost))
+            
+# Busquemos vecinos para cada nodo:
+
+    @property
+    def neighbours(self):
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for edge in self.edges:
+            neighbours[edge.start].add((edge.end, edge.cost))
+
+        return neighbours
+        
+# ¡Es hora del algoritmo! Cambie el nombre de las variables para que sea más fácil de entender.
+
+    def dijkstra(self, source, dest):
+        assert source in self.vertices, 'Such source node doesn\'t exist'
+        distances = {vertex: inf for vertex in self.vertices}
+        previous_vertices = {
+            vertex: None for vertex in self.vertices
+        }
+        distances[source] = 0
+        vertices = self.vertices.copy()
+
+        while vertices:
+            current_vertex = min(
+                vertices, key=lambda vertex: distances[vertex])
+            vertices.remove(current_vertex)
+            if distances[current_vertex] == inf:
+                break
+            for neighbour, cost in self.neighbours[current_vertex]:
+                alternative_route = distances[current_vertex] + cost
+                if alternative_route < distances[neighbour]:
+                    distances[neighbour] = alternative_route
+                    previous_vertices[neighbour] = current_vertex
+
+        path, current_vertex = deque(), dest
+        while previous_vertices[current_vertex] is not None:
+            path.appendleft(current_vertex)
+            current_vertex = previous_vertices[current_vertex]
+        if path:
+            path.appendleft(current_vertex)
+        return path
+
+# uso del algoritmo
+
+graph = Graph([
+    ("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
+    ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
+    ("e", "f", 9)])
+
+print(graph.dijkstra("a", "e"))
